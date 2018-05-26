6e20c31f81718eca4c33bb9a81edd9c1bb63a53c
Draden Gaffney <dsg2dm@virginia.edu>
2018-05-26 16:20:17 -0700

    hello

--- Roomed-master/backend/sublet_api/settings.py
+++ Roomed-master/backend/sublet_api/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-
+#test push
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
